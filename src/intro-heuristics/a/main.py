b7bff590b5df38efaa8f4bb8f8c1128d1efc5650
hrt0809 <hrt.ueda0809@gmail.com>
2022-12-24 02:11:57 +0900

    update: 山登り法(評価値計算工夫なし，貪欲法工夫なし)

--- src/intro-heuristics/a/main.py
+++ src/intro-heuristics/a/main.py
@@ -2,129 +2,177 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import annotations
+
+import abc
+import dataclasses
+import random
 import sys
+import time
+from typing import Final
+
+random.seed(0)  # 調査のためシード値を固定しておく
 
 
-class DecreaseParams:  # 満足度減少パラメータ
-    def __init__(self) -> None:
-        self.__params: dict[int, int] = {}
-
-    def set_params(self) -> None:
-        for i, x in enumerate(sys.stdin.readline().rstrip().split(), 1):
-            self.__params[i] = int(x)
-
-    def get(self, contest_id: int) -> int:
-        return self.__params[contest_id]
-
-
-class IncreaseParams:  # 満足度増加パラメータ
-    def __init__(self, span: int) -> None:
-        self.__span = span
-        self.__params: dict[tuple[int, int], int] = {}
-
-    def set_params(self) -> None:
-        for d in range(1, self.__span + 1):
-            for i, x in enumerate(sys.stdin.readline().rstrip().split(), 1):
-                self.__params[d, i] = int(x)
-
-    def get(self, date: int, contest_id: int) -> int:
-        return self.__params[date, contest_id]
-
-
+@dataclasses.dataclass
 class Environment:  # 問題環境
-    def __init__(self, span: int, num_contests: int) -> None:
-        self.__span = span
-        self.__num_contests = num_contests
-        self.__c = DecreaseParams()
-        self.__s = IncreaseParams(span)
-
-    def get_span(self) -> int:
-        return self.__span
-
-    def get_num_contests(self) -> int:
-        return self.__num_contests
-
-    def set_decrease_params(self) -> None:
-        self.__c.set_params()
-
-    def set_increase_params(self) -> None:
-        self.__s.set_params()
-
-    def get_decrease_params(self) -> DecreaseParams:
-        return self.__c
-
-    def get_increase_params(self) -> IncreaseParams:
-        return self.__s
+    span: Final[int]
+    num_contests: Final[int]
+    params_c: Final[dict[int, int]]  # 満足度減少パラメーター
+    params_s: Final[dict[tuple[int, int], int]]  # 満足度増加パラメーター
 
 
-class InitScheduleGenerator:  # 初期スケジュール生成機能
+class Evaluator:  # スケジュール評価機能
     def __init__(self, env: Environment) -> None:
-        self.span = env.get_span()
-        self.num_contests = env.get_num_contests()
-        self.decrease_params = env.get_decrease_params()
-        self.increase_params = env.get_increase_params()
+        self.env = env
 
-    def get_greedy_schedule(self) -> dict[int, int]:
-        schedule = {}
-        last_dates = {i: 0 for i in range(1, self.num_contests + 1)}
-        for d in range(1, self.span + 1):
+    def calculate_satisfaction(self, schedule: Schedule) -> int:
+        last_dates = {i: 0 for i in range(1, self.env.num_contests + 1)}
+        total_satisfaction = 0
+        for d in range(1, len(schedule) + 1):
+            satisfaction = 0
+            hold_contest_id = schedule[d]
+            # 最後のコンテスト開催日を更新する
+            last_dates[hold_contest_id] = d
+            # 開かれなかったコンテストによる満足度の低下を計算する
+            for i in range(1, self.env.num_contests + 1):
+                satisfaction -= (d - last_dates[i]) * self.env.params_c[i]
+            # 開かれたコンテストによる満足度の増加を足す
+            satisfaction += self.env.params_s[d, hold_contest_id]
+            total_satisfaction += satisfaction
+        return total_satisfaction
+
+
+class Schedule:
+    def __init__(self, env: Environment) -> None:
+        self.env = env
+        self.evaluator = Evaluator(env)
+        self.schedule: dict[int, int] = {}
+        self.satisfaction: int = 0
+        # 貪欲法によるスケジュールを求めておく
+        self.generate_greedy_schedule()
+        self.get_satisfaction()
+
+    def __len__(self) -> int:
+        return len(self.schedule)
+
+    def __getitem__(self, date: int) -> int:
+        return self.schedule[date]
+
+    def print_schedule(self) -> None:
+        for d in range(1, self.env.span + 1):
+            print(self.schedule[d])
+
+    def generate_greedy_schedule(self) -> None:  # 貪欲法によるスケジュール生成
+        last_dates = {i: 0 for i in range(1, self.env.num_contests + 1)}
+        for d in range(1, self.env.span + 1):
             max_satisfaction = -1 * sys.maxsize
             best_contest_id = 1
-            for i in range(1, self.num_contests + 1):
+            for i in range(1, self.env.num_contests + 1):
                 old_last_date = last_dates[i]
                 last_dates[i] = d
-                scr = self.increase_params.get(d, i)
-                scr -= sum(self.decrease_params.get(c) * (d - last_dates[c]) for c in range(1, self.num_contests + 1))
+                scr = self.env.params_s[d, i]
+                scr -= sum(self.env.params_c[j] * (d - last_dates[j]) for j in range(1, self.env.num_contests + 1))
                 if max_satisfaction < scr:
                     max_satisfaction = scr
                     best_contest_id = i
                 last_dates[i] = old_last_date
             last_dates[best_contest_id] = d
-            schedule[d] = best_contest_id
-        return schedule
+            self.schedule[d] = best_contest_id
+
+    def get_satisfaction(self) -> int:
+        self.satisfaction = self.evaluator.calculate_satisfaction(self)
+        return self.satisfaction
+
+    def set_contest(self, date: int, contest: int) -> None:
+        self.schedule[date] = contest
 
 
-# [TODO] 差分計算をして高速化したい
-class Evaluator:  # スケジュール評価機能
+class Neighborhood(metaclass=abc.ABCMeta):
     def __init__(self, env: Environment) -> None:
-        self.num_contests = env.get_num_contests()
-        self.decrease_params = env.get_decrease_params()
-        self.increase_params = env.get_increase_params()
+        self.env = env
+        self.schedule: Schedule
+        self.satisfaction: int
 
-    def calculate_satisfaction(self, schedule: dict[int, int]) -> int:
-        last_dates = {i: 0 for i in range(1, self.num_contests + 1)}
-        total_satisfaction = 0
-        for d in range(1, len(schedule) + 1):
-            score = 0
-            hold_contest_id = schedule[d]
-            # 最後のコンテスト開催日を更新する
-            last_dates[hold_contest_id] = d
-            # 開かれなかったコンテストによる満足度の低下を計算する
-            for i in range(1, self.num_contests + 1):
-                score -= (d - last_dates[i]) * self.decrease_params.get(i)
-            # 開かれたコンテストによる満足度の増加を足す
-            score += self.increase_params.get(d, hold_contest_id)
-            total_satisfaction += score
-        return total_satisfaction
+    def set_schedule(self, schedule: Schedule) -> None:
+        self.schedule = schedule
+        self.satisfaction = self.schedule.satisfaction
+
+    @abc.abstractmethod
+    def execute(self) -> None:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def rollback(self) -> None:
+        raise NotImplementedError()
+
+    def get_satisfaction(self) -> int:
+        return self.schedule.get_satisfaction()
+
+
+class SingleContestExchange(Neighborhood):
+    def __init__(self, env: Environment) -> None:
+        super().__init__(env)
+        self.decided_date: int
+        self.decided_contest: int
+        self.original_contest: int
+
+    def execute(self) -> None:
+        self.decided_date = random.randint(1, self.env.span)
+        self.decided_contest = random.randint(1, self.env.num_contests)
+        self.original_contest = self.schedule[self.decided_date]
+        self.schedule.set_contest(self.decided_date, self.decided_contest)
+
+    def rollback(self) -> None:
+        self.schedule.set_contest(self.decided_date, self.original_contest)
+
+
+def hill_climbing(env: Environment, schedule: Schedule) -> Schedule:
+    TIME_LIMIT = 1.75
+
+    start_time = time.perf_counter()
+    calculation_time = 0.0
+    cnt: int = 1
+    best_score: int = schedule.satisfaction
+    best_schedule: Schedule = schedule
+    neighbor = SingleContestExchange(env)
+    while True:
+        if cnt % 30 == 0:
+            calculation_time = time.perf_counter() - start_time
+        if calculation_time > TIME_LIMIT:
+            # print(calculation_time)
+            break
+        neighbor.set_schedule(best_schedule)
+        neighbor.execute()
+        score = neighbor.get_satisfaction()
+        if score > best_score:
+            # print(f"exchanged!")
+            best_score = score
+            best_schedule = neighbor.schedule
+        else:
+            neighbor.rollback()
+        cnt += 1
+    return best_schedule
 
 
 def main():
-    D = int(input())  # スケジュール生成期間
+    SPAN = int(input())  # スケジュール生成期間
     num_contests = 26  # コンテストは A から Z まで 26個存在する
 
-    env = Environment(D, num_contests)  # 問題環境の構築
-    env.set_decrease_params()  # c: 満足度低下パラメータ の受け取り
-    env.set_increase_params()  # s: 満足度増加パラメータ の受け取り
+    # c: 満足度低下パラメータ の受け取り
+    params_c: dict[int, int] = {}
+    for i, x in enumerate(sys.stdin.readline().rstrip().split(), 1):
+        params_c[i] = int(x)
 
-    evaluator = Evaluator(env)  # スケジュール評価機能の生成
+    # s: 満足度増加パラメータ の受け取り
+    params_s: dict[tuple[int, int], int] = {}
+    for d in range(1, SPAN + 1):
+        for i, x in enumerate(sys.stdin.readline().rstrip().split(), 1):
+            params_s[d, i] = int(x)
 
-    init_schedule_generator = InitScheduleGenerator(env)  # 初期スケジュール生成機能の生成
-    schedule = init_schedule_generator.get_greedy_schedule()  # 初期スケジュール生成
-    evaluator.calculate_satisfaction(schedule)  # スケジュール評価
-
-    # スケジュール表示
-    for d in range(1, D + 1):
-        print(schedule[d])
+    env = Environment(SPAN, num_contests, params_c, params_s)  # 問題環境の構築
+    init_schedule = Schedule(env)  # 初期スケジュール生成
+    best_schedule = hill_climbing(env, init_schedule)
+    best_schedule.print_schedule()  # スケジュール表示
 
 
 if __name__ == "__main__":
